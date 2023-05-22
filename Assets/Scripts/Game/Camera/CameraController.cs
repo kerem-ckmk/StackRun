abc757a9333fd7f3b4e21dd29a00b0a1e4d74a3d
keremcakmakx <95014971+keremcakmakx@users.noreply.github.com>
2023-05-22 19:54:28 +0300

    finish

--- Assets/Scripts/Game/Camera/CameraController.cs
+++ Assets/Scripts/Game/Camera/CameraController.cs
@@ -1,16 +1,22 @@
 using Cinemachine;
+using DG.Tweening;
+using System.Drawing.Drawing2D;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public CinemachineBrain brain;
     public CinemachineVirtualCamera playerCamera;
     public CinemachineVirtualCamera finishCamera;
     public Transform finishCameraTarget;
     public bool IsInitialized { get; private set; }
 
+    private CinemachineBlendDefinition _blend;
+
     private bool _finishGame;
     public void Initialize()
     {
+        _blend = brain.m_DefaultBlend;
         finishCamera.gameObject.SetActive(false);
         IsInitialized = true;
     }
@@ -18,6 +24,10 @@ public class CameraController : MonoBehaviour
     public void Prepare(float finishTargetZ)
     {
         _finishGame = false;
+
+        _blend.m_Time = 0f;
+        brain.m_DefaultBlend = _blend;
+
         finishCameraTarget.transform.eulerAngles = Vector3.zero;
         finishCameraTarget.SetPositionZ(finishTargetZ);
 
@@ -27,9 +37,13 @@ public class CameraController : MonoBehaviour
 
     public void FinishGame()
     {
+        _blend.m_Time = 1f;
+        brain.m_DefaultBlend = _blend;
+
         _finishGame = true;
         playerCamera.gameObject.SetActive(false);
         finishCamera.gameObject.SetActive(true);
+
     }
 
     private void Update()
